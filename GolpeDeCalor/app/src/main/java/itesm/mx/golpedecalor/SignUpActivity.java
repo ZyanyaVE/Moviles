d18d12d8dd52ed0aa01e2f3566873e642c6e4631
Zyanya <zeta_val@hotmail.com>
2015-03-17 22:42:38 -0600

    1.0 - Interface Basica de login y registro

--- GolpeDeCalor/app/src/main/java/itesm/mx/golpedecalor/SignUpActivity.java
+++ GolpeDeCalor/app/src/main/java/itesm/mx/golpedecalor/SignUpActivity.java
@@ -1,39 +1,24 @@
 package itesm.mx.golpedecalor;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    // Declaracion de Variables
-
+public class SignUpActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
     }
 
-    public void onClickIngresar(View v){
-
-    }
-
-    public void onClickRegistrarse(View v){
-        Intent registrarseIntent = new Intent(MainActivity.this, SignUpActivity.class);
-
-        startActivity(registrarseIntent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
 
@@ -43,10 +28,12 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
