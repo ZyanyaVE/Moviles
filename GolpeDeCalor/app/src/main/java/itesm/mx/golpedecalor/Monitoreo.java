c5dba54fdbca948d14fce36e690a207383095bc4
MarceloCQ <macq_94@hotmail.com>
2015-05-06 19:38:50 -0500

    Documentación Terminada

--- GolpeDeCalor/app/src/main/java/itesm/mx/golpedecalor/Monitoreo.java
+++ GolpeDeCalor/app/src/main/java/itesm/mx/golpedecalor/Monitoreo.java
@@ -1,3 +1,30 @@
+/*
+    Golpe de Calor
+
+    Copyright (C) 2015
+    Marcelo Alberto Cantú Quiroga
+    Zyanya Valdés Esquivel
+    Hugo León Garza
+
+    Última Modificación: 28 de Abril del 2015
+    Nombre del Archivo: Monitoreo.java
+    Convención de nombres: "CamelCase"
+    Versión 3.0
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package itesm.mx.golpedecalor;
 
 import android.os.Handler;
@@ -7,9 +34,6 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * Created by Marcelo on 15/04/2015.
- */
 public class Monitoreo {
     Grupo grupo;
     MonitoringActivity interfaz;
